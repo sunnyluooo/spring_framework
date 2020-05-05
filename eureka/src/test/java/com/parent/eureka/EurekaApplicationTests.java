00c20d850c1e8551568ccaaae3ce5214d08c5054
trayvonn <513168326@qq.com>
2020-05-05 17:41:58 +0800

    init

---
+++ eureka/src/test/java/com/parent/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parent.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka2/src/main/java/com/parent/eureka2/Eureka2Application.java
@@ -0,0 +1,13 @@
+package com.parent.eureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka2Application.class, args);
+    }
+
+}
