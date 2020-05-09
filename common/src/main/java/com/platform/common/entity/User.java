21336767b1e3867060a5dc84f7d5405f2b65e879
trayvonn <513168326@qq.com>
2020-05-09 11:56:10 +0800

    add modules `common`

---
+++ common/src/main/java/com/platform/common/entity/User.java
@@ -0,0 +1,10 @@
+package com.platform.common.entity;
+
+/**
+ * @author lcw
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2020-05-09
+ */
+public class User {
+}
---
+++ common/src/main/java/com/platform/common/vo/UserVO.java
@@ -0,0 +1,10 @@
+package com.platform.common.vo;
+
+/**
+ * @author lcw
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2020-05-09
+ */
+public class UserVO {
+}
