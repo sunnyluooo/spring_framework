d03f814e20e227257a364e97f33708ff1386908e
trayvonn <513168326@qq.com>
2020-06-11 14:12:50 +0800

    add  config module

--- admin/src/main/java/com/platform/admin/controller/ApiController.java
+++ admin/src/main/java/com/platform/admin/controller/ApiController.java
@@ -1,5 +1,7 @@
 package com.platform.admin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description TODO
  * @createTime 2020-05-07
  */
+@RefreshScope
 @RequestMapping("/api")
 @RestController
 public class ApiController {
+
+    @Value("${test}")
+    private String test;
 
     @GetMapping("hello")
     public String hello(){
         Authentication authentication = SecurityContextHolder.getContext()
                 .getAuthentication();
         return "hello "+authentication.getName();
+    }
+
+    @GetMapping("test")
+    public String test(){
+        return test;
     }
 }
--- config/src/main/java/com/platform/config/ConfigApplication.java
+++ config/src/main/java/com/platform/config/ConfigApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import javax.security.auth.login.Configuration;
-
 /**
  * @author: 吴邪
  * @date: 2020/6/10 14:32
@@ -15,6 +13,6 @@ import javax.security.auth.login.Configuration;
 public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Configuration.class,args);
+        SpringApplication.run(ConfigApplication.class,args);
     }
 }
