a4e50c9325305899d679182f90f6665089b46b57
trayvonn <513168326@qq.com>
2020-08-04 09:14:02 +0800

    submit spring cloud gateway module

---
+++ gateway/src/main/java/com/platform/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.platform.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 吴邪
+ * @date: 2020/8/3 11:06
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
---
+++ gateway/src/main/java/com/platform/gateway/config/RouteConfig.java
@@ -0,0 +1,12 @@
+package com.platform.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: 吴邪
+ * @date: 2020/7/31 20:26
+ */
+@Configuration
+public class RouteConfig {
+
+}
