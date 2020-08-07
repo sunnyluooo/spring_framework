6ad09517061e5435a3c618d869af940102f238ea
trayvonn <513168326@qq.com>
2020-08-07 14:41:02 +0800

    增加公共常量表

---
+++ admin/admin-service/src/main/java/com/platform/service/admin/constant/ConstantPublic.java
@@ -0,0 +1,11 @@
+package com.platform.service.admin.constant;
+
+/**
+ * 公共常量表
+ * @author 吴邪
+ * @date 2020/8/7 14:23
+ */
+public class ConstantPublic {
+    public static final Integer YES = 1;
+    public static final Integer NO = 0;
+}
