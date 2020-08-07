031a9ad6068d12c7026dae0dd619cc74e3a87809
trayvonn <513168326@qq.com>
2020-08-07 14:51:32 +0800

    增加Test类

---
+++ admin/admin-service/src/main/java/com/platform/service/admin/service/TestService.java
@@ -0,0 +1,8 @@
+package com.platform.service.admin.service;
+
+/**
+ * @author 吴邪
+ * @date 2020/8/7 14:49
+ */
+public interface TestService {
+}
---
+++ admin/admin-service/src/main/java/com/platform/service/admin/service/impl/TestServiceImpl.java
@@ -0,0 +1,10 @@
+package com.platform.service.admin.service.impl;
+
+import com.platform.service.admin.service.TestService;
+
+/**
+ * @author 吴邪
+ * @date 2020/8/7 14:49
+ */
+public class TestServiceImpl implements TestService {
+}
