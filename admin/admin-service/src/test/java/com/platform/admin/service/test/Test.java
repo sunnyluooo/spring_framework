4d6845c894e67e4674f2dfea39e522581c72f6a5
trayvonn <513168326@qq.com>
2020-08-11 16:59:55 +0800

    修改eureka配置

---
+++ admin/admin-service/src/test/java/com/platform/admin/service/test/Test.java
@@ -0,0 +1,14 @@
+package com.platform.admin.service.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 吴邪
+ * @date 2020/8/11 16:18
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test {
+}
